b91f20f2e7db585610ea9384ddd6f432d2171b92
Mary <maryandreeva100@gmail.com>
2023-03-13 01:38:11 +0300

    Random number

---
+++ 4.4/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Написать функцию, которая принимает min и max и возвращает случайное число между ними, включая их */
+
+function random(min, max) {
+  return Math.floor((Math.random() * (max - min + 1) + min))
+};
+
+console.log(random(1, 2));
+
+
