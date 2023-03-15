7dbda81149294449b0b11fe253cd42f5d12f7e24
Mary <maryandreeva100@gmail.com>
2023-03-15 22:21:21 +0300

    Strike to enemy

---
+++ 9.5/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/* 
+  Сделать класс врага со здоровьем и методом получения урона. 
+  Сделать класс меча, который имеет силу и метод нанесения урона. 
+  Сделать кдасс орка, который в 50% случаев не получает урон. 
+*/
+
+class Enemy {
+  health;
+  constructor(health) {
+    this.health = health;
+  }
+
+  recieveDamage(damage) {
+    this.health = this.health - damage;
+    console.log(this.health);
+  }
+}
+
+class Sword {
+  #damage;
+  constructor(damage) {
+    this.#damage = damage;
+  }
+
+  strike(enemy) {
+    enemy.recieveDamage(this.#damage);
+  }
+}
+
+class Orc extends Enemy {
+  constructor(health) {
+    super(health);
+  }
+
+  recieveDamage(damage) {
+    if(Math.random() > 0.5) {
+      this.health = this.health - damage;
+    }
+    console.log(this.health);
+  }
+}
+
+const enemy = new Orc(10);
+const sword = new Sword(3);
+sword.strike(enemy);
+sword.strike(enemy);
+sword.strike(enemy);
